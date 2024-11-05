3adb84f909984eb47541a1f3bc79ae58364c106f
Adam Raine <6752989+adamraine@users.noreply.github.com>
2024-11-04 18:21:34 -0800

    core(target-manager): ignore target if type is unknown (#16221)

--- core/gather/driver/target-manager.js
+++ core/gather/driver/target-manager.js
@@ -170,6 +170,10 @@ class TargetManager extends ProtocolEventEmitter {
       // Sometimes targets can be closed before we even have a chance to listen to their network activity.
       if (/Target closed/.test(err.message)) return;
 
+      // `Target.getTargetInfo` is not implemented for certain target types.
+      // Lighthouse isn't interested in these targets anyway so we can just ignore them.
+      if (/'Target.getTargetInfo' wasn't found/.test(err)) return;
+
       // Worker targets can be a bit fickle and we only enable them for diagnostic purposes.
       // We shouldn't throw a fatal error if there were issues attaching to them.
       if (targetType === 'worker') {
--- core/test/gather/driver/target-manager-test.js
+++ core/test/gather/driver/target-manager-test.js
@@ -125,6 +125,21 @@ describe('TargetManager', () => {
       expect(sendMock.findAllInvocations('Runtime.runIfWaitingForDebugger')).toHaveLength(1);
     });
 
+    it('should ignore errors if Target.getTargetInfo is undefined', async () => {
+      targetInfo.type = 'worker';
+      sendMock
+        .mockResponse('Target.getTargetInfo', () => {
+          throw new Error(`'Target.getTargetInfo' wasn't found`);
+        });
+      await targetManager.enable();
+
+      const invocations = sendMock.findAllInvocations('Target.setAutoAttach');
+      expect(invocations).toHaveLength(0);
+
+      // Should still be resumed.
+      expect(sendMock.findAllInvocations('Runtime.runIfWaitingForDebugger')).toHaveLength(1);
+    });
+
     it('should ignore targets that are not frames or web workers', async () => {
       targetInfo.type = 'service_worker';
       sendMock
