4397883a1ecb63e439947d834e662efb1691388a
Adam Raine <6752989+adamraine@users.noreply.github.com>
2025-03-03 15:56:39 -0800

    core(image-delivery-insight): use strings without savings (#16364)

--- core/audits/insights/image-delivery-insight.js
+++ core/audits/insights/image-delivery-insight.js
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {UIStrings} from '@paulirish/trace_engine/models/trace/insights/ImageDelivery.js';
+import {UIStrings, ImageOptimizationType} from '@paulirish/trace_engine/models/trace/insights/ImageDelivery.js';
 
 import {Audit} from '../audit.js';
 import * as i18n from '../../lib/i18n/i18n.js';
@@ -35,6 +35,33 @@ class ImageDeliveryInsight extends Audit {
   }
 
   /**
+   * Note: This function is a copy of the `getOptimizationMessage` function found in the imported
+   * module. We could re-use the output of that function but it's output is shimmed to a {i18nId, values} object
+   * which is not consistent with the TS return type.
+   *
+   * We also can't change the function to output the untranslated strings because the responsive
+   * size string has placeholders that need to be resolved here.
+   *
+   * @param {import('@paulirish/trace_engine/models/trace/insights/ImageDelivery.js').ImageOptimization} optimization
+   * @returns
+   */
+  static getOptimizationMessage(optimization) {
+    switch (optimization.type) {
+      case ImageOptimizationType.ADJUST_COMPRESSION:
+        return str_(UIStrings.useCompression);
+      case ImageOptimizationType.MODERN_FORMAT_OR_COMPRESSION:
+        return str_(UIStrings.useModernFormat);
+      case ImageOptimizationType.VIDEO_FORMAT:
+        return str_(UIStrings.useVideoFormat);
+      case ImageOptimizationType.RESPONSIVE_SIZE:
+        return str_(UIStrings.useResponsiveSize, {
+          PH1: `${optimization.fileDimensions.width}x${optimization.fileDimensions.height}`,
+          PH2: `${optimization.displayDimensions.width}x${optimization.displayDimensions.height}`,
+        });
+    }
+  }
+
+  /**
    * @param {LH.Artifacts} artifacts
    * @param {LH.Audit.Context} context
    * @return {Promise<LH.Audit.Product>}
@@ -45,10 +72,6 @@ class ImageDeliveryInsight extends Audit {
         // TODO: show UIStrings.noOptimizableImages?
         return;
       }
-
-      const relatedEventsMap = insight.relatedEvents && !Array.isArray(insight.relatedEvents) ?
-        insight.relatedEvents :
-        null;
 
       /** @type {LH.Audit.Details.Table['headings']} */
       const headings = [
@@ -66,11 +89,9 @@ class ImageDeliveryInsight extends Audit {
         wastedBytes: image.byteSavings,
         subItems: {
           type: /** @type {const} */ ('subitems'),
-          // TODO: when strings update to remove number from "reason" uistrings, update this
-          // to use `image.optimizations.map(...)` and construct strings from the type.
-          items: (relatedEventsMap?.get(image.request) ?? []).map((reason, i) => ({
-            reason,
-            wastedBytes: image.optimizations[i].byteSavings,
+          items: image.optimizations.map(optimization => ({
+            reason: this.getOptimizationMessage(optimization),
+            wastedBytes: optimization.byteSavings,
           })),
         },
       }));
