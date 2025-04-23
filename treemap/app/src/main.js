dfa7d241341a198e1e006cf44fd6d497748758a2
Connor Clark <cjamcl@google.com>
2025-04-23 14:10:56 -0700

    misc(treemap): parse options properly from hash params (#16459)

--- treemap/app/src/main.js
+++ treemap/app/src/main.js
@@ -865,18 +865,11 @@ class LighthouseTreemap {
       throw new Error('provided Lighthouse result is missing audit: `script-treemap-data`');
     }
 
-    if (lhr === json.lhr) {
-      // Special case: file was {lhr: ...} and potentially has other properties.
-      // LH.Treemap.Options only has `lhr` currently, but may have more in the future.
-      return json;
-    }
-
     let initialView;
-    if (typeof json.initialView === 'string') {
+    if (json && typeof json === 'object' && typeof json.initialView === 'string') {
       initialView = json.initialView;
     }
 
-    // json was exactly a LHR, or a PSI result object aka {lighthouseResult}
     return {lhr, initialView};
   }
 
@@ -987,7 +980,7 @@ async function main() {
     const options = app.coerceToOptions(json);
     app.init(options);
   } else if (params.lhr) {
-    const options = app.coerceToOptions(params.lhr);
+    const options = app.coerceToOptions(params);
     app.init(options);
   } else if (params.gist) {
     const json = await app._github.getGistFileContentAsJson(params.gist || '');
