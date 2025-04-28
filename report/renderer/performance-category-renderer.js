16c9576cba64f923f9a976e90aa7c9acaaa6c387
Connor Clark <cjamcl@google.com>
2025-04-28 11:52:21 -0700

    report: remove innerHTML usage from insights announce icon (#16469)

--- report/renderer/performance-category-renderer.js
+++ report/renderer/performance-category-renderer.js
@@ -194,8 +194,7 @@ export class PerformanceCategoryRenderer extends CategoryRenderer {
     // Insights / Audits toggle.
     const container = this.dom.createChildOf(element, 'div', 'lh-perf-insights-toggle');
     const textSpan = this.dom.createChildOf(container, 'span', 'lh-perf-toggle-text');
-    const icon = this.dom.createElement('span', 'lh-perf-insights-icon');
-    icon.innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M18 13V11H22V13H18ZM19.2 20L16 17.6L17.2 16L20.4 18.4L19.2 20ZM17.2 8L16 6.4L19.2 4L20.4 5.6L17.2 8ZM5 19V15H4C3.45 15 2.975 14.8083 2.575 14.425C2.19167 14.025 2 13.55 2 13V11C2 10.45 2.19167 9.98333 2.575 9.6C2.975 9.2 3.45 9 4 9H8L13 6V18L8 15H7V19H5ZM11 14.45V9.55L8.55 11H4V13H8.55L11 14.45ZM14 15.35V8.65C14.45 9.05 14.8083 9.54167 15.075 10.125C15.3583 10.6917 15.5 11.3167 15.5 12C15.5 12.6833 15.3583 13.3167 15.075 13.9C14.8083 14.4667 14.45 14.95 14 15.35Z"/></svg>';
+    const icon = this.dom.createElement('span', 'lh-perf-insights-icon insights-icon-url');
     textSpan.appendChild(icon);
     textSpan.appendChild(this.dom.convertMarkdownLinkSnippets(Globals.strings.insightsNotice));
 
