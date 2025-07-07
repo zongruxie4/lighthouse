6365a4a5e9854b8acb794b284d8f358fdcff98a8
Connor Clark <cjamcl@google.com>
2025-07-07 12:53:36 -0700

    tests: update expectations (#16580)

--- third-party/devtools-tests/e2e/lighthouse/navigation_test.ts
+++ third-party/devtools-tests/e2e/lighthouse/navigation_test.ts
@@ -120,7 +120,7 @@ describe('Navigation', function() {
     });
 
     const {auditResults, erroredAudits, failedAudits} = getAuditsBreakdown(lhr, ['max-potential-fid']);
-    assert.strictEqual(auditResults.length, 175);
+    assert.strictEqual(auditResults.length, 176);
     assert.deepStrictEqual(erroredAudits, []);
     assert.deepStrictEqual(failedAudits.map(audit => audit.id), [
       'document-title',
@@ -205,7 +205,7 @@ describe('Navigation', function() {
     ];
 
     const {auditResults, erroredAudits, failedAudits} = getAuditsBreakdown(lhr, flakyAudits);
-    assert.strictEqual(auditResults.length, 175);
+    assert.strictEqual(auditResults.length, 176);
     assert.deepStrictEqual(erroredAudits, []);
     assert.deepStrictEqual(failedAudits.map(audit => audit.id), [
       'document-title',
